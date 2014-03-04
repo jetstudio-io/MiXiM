0ecdf74d3cfa46d9aaab756e783a25bd0477bf62
NGUYEN Van Thiep <nvt2302@gmail.com>
2014-03-04 10:22:38 +0100

    Remove test in BMAC

--- src/modules/mac/BMacLayer.cc
+++ src/modules/mac/BMacLayer.cc
@@ -3,15 +3,15 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 #include "BMacLayer.h"
 
@@ -68,7 +68,6 @@ void BMacLayer::initialize(int stage)
 		droppedPacket.setReason(DroppedPacket::NONE);
 		nicId = getNic()->getId();
 		WATCH(macState);
-		std::cout << checkInterval << std::endl;
 	}
 	else if(stage == 1) {
 		wakeup = new cMessage("wakeup");
