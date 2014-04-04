2c7b653788413c57d271cece15066cf1caf25991
NGUYEN Van Thiep <nvt2302@gmail.com>
2014-04-04 16:12:32 +0200

    Test 100 times for 1.0 1.0 1.0 1.0

--- examples/tadmac/run_batch.sh
+++ examples/tadmac/run_batch.sh
@@ -2,8 +2,8 @@
 for i in {0..99}
 do
 	if [ $i -lt 10 ]; then
-		../miximexamples -r 0 -n ..:../../src/base:../../src/inet_stub:../../src/modules --tkenv-image-path=../../images -l ../../src/mixim --output-scalar-file=results/4/0$i.sca -u Cmdenv omnetpp.ini
+		../miximexamples -r 0 -n ..:../../src/base:../../src/inet_stub:../../src/modules --tkenv-image-path=../../images -l ../../src/mixim --output-scalar-file=results/5/0$i.sca -u Cmdenv omnetpp.ini
 	else
-		../miximexamples -r 0 -n ..:../../src/base:../../src/inet_stub:../../src/modules --tkenv-image-path=../../images -l ../../src/mixim --output-scalar-file=results/4/$i.sca -u Cmdenv omnetpp.ini
+		../miximexamples -r 0 -n ..:../../src/base:../../src/inet_stub:../../src/modules --tkenv-image-path=../../images -l ../../src/mixim --output-scalar-file=results/5/$i.sca -u Cmdenv omnetpp.ini
 	fi
 done
